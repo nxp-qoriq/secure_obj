4fafd61c994c5764b9f4fc377dd531d95c90bace
Sumit Garg <sumit.garg@nxp.com>
2017-12-01 01:57:05 +0530

    TA: Add sub.mk for compilation
    Signed-off-by: Sumit Garg <sumit.garg@nxp.com>

--- secure_storage_ta/ta/sub.mk
+++ secure_storage_ta/ta/sub.mk
@@ -1,2 +1,7 @@
 global-incdirs-y += include
 srcs-y += secure_storage_ta.c
+srcs-y += secure_storage_db.c
+srcs-y += secure_storage_create.c
+srcs-y += secure_storage_find.c
+srcs-y += secure_storage_helper.c
+srcs-y += secure_storage_rsa.c
